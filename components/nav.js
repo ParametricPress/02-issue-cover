20ff911b80ccbfe013c54750124b19fd665cda5b
Fred Hohman <fred.hohman@gmail.com>
2020-10-15 14:40:02 -0400

    cover updates: * reorder articles to match TOC order * switch to dark mode * add editor's note link

--- components/nav.js
+++ components/nav.js
@@ -30,7 +30,7 @@ class Nav extends React.PureComponent {
     return (
       <div className="parametric-article-nav">
           <div>
-            <div style={{fontFamily:'Graphik Web', textDecoration: 'none'}}>Parametric Press</div>
+          <div style={{ fontFamily: 'Graphik Web', textDecoration: 'none', color: "#ADADAD"}}>Parametric Press</div>
           </div>
           <div className="parametric-nav-links" style={{
             display: 'flex',
